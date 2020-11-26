8e69b0733c769db7a3fe44b1cfc35222ecddadfb
Iman Heizer Pua <imanheizer@gmail.com>
2020-11-26 14:40:25 +0800

    Preprocessor updated

--- core/preprocessor.py
+++ core/preprocessor.py
@@ -5,6 +5,7 @@ import os
 import cv2
 import pandas as pd
 import numpy as np
+import shutil
 
 
 class preprocessor():
@@ -64,66 +65,89 @@ class preprocessor():
         microXoutput = dataFolder + "/" + folderName
         microX = ['leftEye', 'rightEye', 'leftBrow', 'rightBrow', 'mouth', 'nose' ]
         for directory in microX:
-            os.mkdir(os.path.join(microXoutput,directory))
+            try:
+                os.mkdir(os.path.join(microXoutput,directory))
+            except:
+                print("Folders already exists")
+                break
         
         keypoints = pd.read_csv(CSV)
         keypoints.set_index('filename', inplace=True)
         dim = (60, 60)
+        filenames = []
 
         for filename in os.listdir(framesFolder):
             if filename == ".DS_Store":
                 continue 
-            image = cv2.imread(os.path.join(framesFolder,filename))
-            print(filename)
+            filenames.append(filename)
 
-            if keypoints.loc[filename, " success"] == 0:
-                continue
+        while len(filenames) > 0:
+            print(filenames)
+            for filename in filenames:
+                image = cv2.imread(os.path.join(framesFolder,filename))
+                print(filename)
 
-            for microXcomponent in microX:
-                if microXcomponent == "leftEye":
-                    beginX = keypoints.loc[filename,' x_36']
-                    endX = keypoints.loc[filename,' x_39']
-                    beginY = keypoints.loc[filename,' y_38']
-                    endY = keypoints.loc[filename,' y_40']
-                    # beginY = np.argmin(keypoints.loc[filename,' y_37'], keypoints.loc[filename,' y_38'])
-                    # endY = np.argmax(keypoints.loc[filename,' y_40'], keypoints.loc[filename,' y_41'])
-
-                if microXcomponent == "rightEye":
-                    beginX = keypoints.loc[filename,' x_42']
-                    endX = keypoints.loc[filename,' x_45']
-                    beginY = keypoints.loc[filename,' y_44']
-                    endY = keypoints.loc[filename,' y_46']
-
-                if microXcomponent == "leftbrow":
-                    beginX = keypoints.loc[filename,' x_17']
-                    endX = keypoints.loc[filename,' x_21']
-                    beginY = keypoints.loc[filename,' y_19']
-                    endY = keypoints.loc[filename,' y_17']
-
-                if microXcomponent == "rightbrow":
-                    beginX = keypoints.loc[filename,' x_22']
-                    endX = keypoints.loc[filename,' x_26']
-                    beginY = keypoints.loc[filename,' y_24']
-                    endY = keypoints.loc[filename,' y_26']
-
-                if microXcomponent == "nose":
-                    beginX = keypoints.loc[filename,' x_31']
-                    endX = keypoints.loc[filename,' x_35']
-                    beginY = keypoints.loc[filename,' y_27']
-                    endY = keypoints.loc[filename,' y_33']
-
-                if microXcomponent == "mouth":
-                    beginX = keypoints.loc[filename,' x_48']
-                    endX = keypoints.loc[filename,' x_54']
-                    beginY = keypoints.loc[filename,' y_51']
-                    endY = keypoints.loc[filename,' y_57']
-
-                #TODO change the y points cos we need argmin/argmax instead
-
-                # crop and resize image
-                try:
-                    cropped_image = image[int(beginX):int(endX),int(beginY):int(endY)]
-                    resized = cv2.resize(cropped_image,dim)
-                except:
+                if keypoints.loc[filename, " success"] == 0:
                     continue
-                cv2.imwrite("{}/{}/{}{}".format(microXoutput,microXcomponent, microXcomponent, filename), resized)
+
+    # TODO change the y points cos we need argmin/argmax instead
+
+                for microXcomponent in microX:
+                    if microXcomponent == "leftEye":
+                        beginX = keypoints.loc[filename,' x_36']
+                        endX = keypoints.loc[filename,' x_39']
+                        beginY = keypoints.loc[filename,' y_38']
+                        endY = keypoints.loc[filename,' y_40']
+                        # beginY = np.argmin(keypoints.loc[filename,' y_37'], keypoints.loc[filename,' y_38'])
+                        # endY = np.argmax(keypoints.loc[filename,' y_40'], keypoints.loc[filename,' y_41'])
+
+                    if microXcomponent == "rightEye":
+                        beginX = keypoints.loc[filename,' x_42']
+                        endX = keypoints.loc[filename,' x_45']
+                        beginY = keypoints.loc[filename,' y_44']
+                        endY = keypoints.loc[filename,' y_46']
+
+                    if microXcomponent == "leftbrow":
+                        beginX = keypoints.loc[filename,' x_17']
+                        endX = keypoints.loc[filename,' x_21']
+                        beginY = keypoints.loc[filename,' y_19']
+                        endY = keypoints.loc[filename,' y_17']
+
+                    if microXcomponent == "rightbrow":
+                        beginX = keypoints.loc[filename,' x_22']
+                        endX = keypoints.loc[filename,' x_26']
+                        beginY = keypoints.loc[filename,' y_24']
+                        endY = keypoints.loc[filename,' y_26']
+
+                    if microXcomponent == "nose":
+                        beginX = keypoints.loc[filename,' x_31']
+                        endX = keypoints.loc[filename,' x_35']
+                        beginY = keypoints.loc[filename,' y_27']
+                        endY = keypoints.loc[filename,' y_33']
+
+                    if microXcomponent == "mouth":
+                        beginX = keypoints.loc[filename,' x_48']
+                        endX = keypoints.loc[filename,' x_54']
+                        beginY = keypoints.loc[filename,' y_51']
+                        endY = keypoints.loc[filename,' y_57']
+
+
+                    # crop and resize image
+                    try:
+                        cropped_image = image[int(beginX):int(endX),int(beginY):int(endY)]
+                        resized = cv2.resize(cropped_image,dim)
+                        cv2.imwrite("{}/{}/{}{}".format(microXoutput,microXcomponent, microXcomponent, filename), resized)
+                        try:
+                            filenames.remove(filename)
+                        except:
+                            continue
+                    except:
+                        print("File may have errors")
+                        try:
+                            print("Duplication of previous frames in process")
+                            previous_filename = filename[0:-5] + str(int(filename[-5])-1) + ".jpg"
+                            previous_file_directory = "{}/{}/{}{}".format(microXoutput,microXcomponent, microXcomponent, previous_filename)
+                            shutil.copyfile(previous_file_directory, "{}/{}/{}{}".format(microXoutput,microXcomponent, microXcomponent, filename))
+                            filenames.remove(filename)
+                        except:
+                            continue
